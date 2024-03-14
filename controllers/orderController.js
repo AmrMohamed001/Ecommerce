c51ec0e51d31798b6ee0af889cbb98d5f7d5335e
Amr mohamed <amrmohamd229@gmail.com>
2024-03-14 16:37:33 +0200

    error

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -145,11 +145,11 @@ exports.webhookCheckout = catchAsync(async (req, res, next) => {
 	}
 	if (event.type === 'checkout.session.completed') {
 		console.log('order should created here')
-		console.log(event.data.amount_total) //total price
-		console.log(event.data.customer_email) //user email
-		console.log(event.data.metadata) //address
-		console.log(event.data.payment_method_types) //address
-		console.log(event.data.payment_status) //address
-		console.log(event.data.status) //address
+		console.log(event.data.object.amount_total) //total price
+		console.log(event.data.object.customer_email) //user email
+		console.log(event.data.object.metadata) //address
+		console.log(event.data.object.payment_method_types) //address
+		console.log(event.data.object.payment_status) //address
+		console.log(event.data.object.status) //address
 	}
 })
